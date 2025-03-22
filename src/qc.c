ce92c7d0f1058a1295e213f9f8a4befe172ef6d2
tobi <157970333+tobiKaboom@users.noreply.github.com>
2025-03-22 16:26:32 +0100

    added segfault prevention

--- src/qc.c
+++ src/qc.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void helpmessage() {
+	printf("\033[1;0m\n");
+	printf("  ,___________,\t \033[1;33mqc - quickcalc 0.2\033[1;0m\n");
+	printf("  | ._______. |\t usage: qc [flag] [a] [b]\n");
+	printf("  | |       | |\t flags:\n");
+	printf("  | '-------' |\t -h: displays this help message\n");
+	printf("  |  7 8 9\033[1;33m +\033[1;0m  |\t -p: sums [a] and [b]\n");
+	printf("  |  4 5 6\033[1;33m -\033[1;0m  |\t -m: subtracs [b] from [a]\n");
+	printf("  |  1 2 3\033[1;33m *\033[1;0m  |\t -t: multiplies [a] and [b]\n");
+	printf("  |  0 \033[1;33m. = : \033[1;0m |\t -d: divides [a] by [b]\n");
+	printf("  '-----------'\t -w: raises [a] to the [b]th power\n");
+	printf("               \t\t ~ascii art by me\n\n");
+}
+
 void power(double l1, double l2) {
 	long i;
         double p;
@@ -12,23 +26,15 @@ void power(double l1, double l2) {
 }
 
 int main(int argc, char *argv[]) {
-	if (argv[1][0] == '-') {
+	if (argc < 2)
+		helpmessage();
+	else if (argv[1][0] == '-') {
 		char* end;
 		double n1;
 		double n2;
 		switch (argv[1][1]) {
 			case 'h':
-				printf("\033[1;0m\n");
-				printf("  ,___________,\t \033[1;33mqc - quickcalc 0.2\033[1;0m\n");
-				printf("  | ._______. |\t usage: qc [flag] [a] [b]\n");
-				printf("  | |       | |\t flags:\n");
-				printf("  | '-------' |\t -h: displays this help message\n");
-				printf("  |  7 8 9\033[1;33m +\033[1;0m  |\t -p: sums [a] and [b]\n");
-				printf("  |  4 5 6\033[1;33m -\033[1;0m  |\t -m: subtracs [b] from [a]\n");
-				printf("  |  1 2 3\033[1;33m *\033[1;0m  |\t -t: multiplies [a] and [b]\n");
-				printf("  |  0 \033[1;33m. = : \033[1;0m |\t -d: divides [a] by [b]\n");
-				printf("  '-----------'\t -w: raises [a] to the [b]th power\n");
-				printf("               \t\t ~ascii art by me\n\n");
+				helpmessage();	
 				break; 
 			case 'p':
 				n1 = strtod(argv[2], &end);
@@ -59,5 +65,6 @@ int main(int argc, char *argv[]) {
 				printf("unknown flag\n");
 				break;
 		}
-	}
+	} else
+		helpmessage();
 }
