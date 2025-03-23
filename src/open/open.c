a426fd2a3afe45616f870bccb6209bec5854f29d
tobi <157970333+tobiKaboom@users.noreply.github.com>
2025-03-23 13:08:25 +0100

    added segfault prevention

--- src/open/open.c
+++ src/open/open.c
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include "openconf.h"
 
+void helpmessage() {
+	printf("  \n");
+	printf("        __________ \t\033[1;33mopen 1.0 - opens your files with what you want!\033[1;0m\n");
+	printf("      /          /|\tusage: open [optional flags] [filename.extension]\n");
+	printf("    /__________/  |\t\n");
+	printf("    |_________ |  |\tvalid flags:\n");
+	printf("   /--._____ /||  |\t-h: show this help message\n");
+	printf("  |\"._____.\"| ||  |\t\n");
+	printf("  '_________'/_|  |\t\t~ascii art by Cristopher Johnson\n");
+	printf("    ||\".____.\"||  /\t\n");
+	printf("    |'--------'| / \t\n");
+	printf("    '----------'/  \t\n  \n");
+
+}
+
 void* getextension(char fullfilename[]) {
 	static char extension[128];
 	char dotenc = 0;
@@ -38,32 +53,24 @@ void openfilewith(char fullfile[], char extension[]) {
 }
 
 int main(int argc, char *argv[]) {
-	int filepos;
-	filepos = 1;
+	int filepos = 1;
 	char filenameincluded = 1;
 
-	if (argc < 2)
+	if (argc < 2) {
 		filenameincluded = 0;
-	
+		helpmessage();
+		return 1;
+	}
+
 	if (argv[1][0] == '-') {
 		switch (argv[1][1]) {
 			case 'h':
-				printf("  \n");
-				printf("        __________ \t\033[1;33mopen 1.0 - opens your files with what you want!\033[1;0m\n");
-				printf("      /          /|\tusage: open [optional flags] [filename.extension]\n");
-				printf("    /__________/  |\t\n");
-				printf("    |_________ |  |\tvalid flags:\n");
-				printf("   /--._____ /||  |\t-h: show this help message\n");
-				printf("  |\"._____.\"| ||  |\t\n");
-				printf("  '_________'/_|  |\t\t~ascii art by Cristopher Johnson\n");
-				printf("    ||\".____.\"||  /\t\n");
-				printf("    |'--------'| / \t\n");
-				printf("    '----------'/  \t\n  \n");
-				break;
+				helpmessage();	
+				return 0;
 			default:
 				printf("unrecognized flag\n");
 				printf("run open -h for help\n");
-				break;
+				return 1;
 		}
 		if (argc < 3)
 			filenameincluded = 0;
@@ -74,4 +81,6 @@ int main(int argc, char *argv[]) {
 		char* extension = getextension(argv[filepos]);
 		openfilewith(argv[filepos], extension); 
 	}
+
+	return 0;
 }
